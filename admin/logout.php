2169c1b7e2c643797b7640aa5593bd10b264d93d
Olivier Colas <66916000+olimcfly@users.noreply.github.com>
2026-04-02 17:30:46 +0200

    Harden admin logout session teardown

--- admin/logout.php
+++ admin/logout.php
@@ -1,5 +1,25 @@
 <?php
+
+declare(strict_types=1);
+
 session_start();
+
+$_SESSION = [];
+
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params['path'],
+        $params['domain'],
+        (bool) $params['secure'],
+        (bool) $params['httponly']
+    );
+}
+
 session_destroy();
-header('Location: /admin/login.php');
+
+header('Location: /admin/login.php?logout=1');
 exit;
