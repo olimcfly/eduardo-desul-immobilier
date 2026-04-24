91a3720e1f5ae651176d68a10e817b7641516df9
Olivier Colas <oliviercolas83@gmail.com>
2026-04-24 03:04:08 +0200

    Update admin password hash

--- includes/config.php
+++ includes/config.php
@@ -2,6 +2,6 @@
 declare(strict_types=1);
 
 define('ADMIN_EMAIL', 'contact@eduardo-desul-immobilier.fr');
-define('ADMIN_PASSWORD_HASH', '$2y$10$LGDaPgh9xZWvGc4r76o0W.iiJEv9GIkWm/RUVpgW8girGh2ZyPP/G');
+define('ADMIN_PASSWORD_HASH', '$2y$10$.fnPzwk4dqoaVGekd8GUVuFCdwAXqA3VQeJEgfW1wXeok94eeEpNu');
 define('ADMIN_PASSWORD_RESET_REQUIRED', false);
 
